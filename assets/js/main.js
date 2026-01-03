6d58baa90372f9e1384695d0178f974649c12046
robertyoung12-cyber <robertyoung12@gmail.com>
2026-01-03 13:59:44 -0500

    Update Faithlife link to jcc-31773

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var faithlifeUrl = 'https://faithlife.com';
+  var faithlifeUrl = 'https://faithlife.com/jcc-31773';
   var ids = ['faithlifeLink', 'faithlifeLink2', 'faithlifeLink3'];
   ids.forEach(function(id) {
     var link = document.getElementById(id);
